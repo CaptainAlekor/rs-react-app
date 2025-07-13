5966b15fd7823e6b854a7b023fbe15f36bb03b6f
CaptainAlekor <sashakorostelev49@gmail.com>
2025-07-13 21:28:39 +0300

    feat: add error boundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+class ErrorBoundary extends React.Component<
+  { fallback: React.ReactNode; children: React.ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    console.log([error.name, error.message, new Date().toString()].join('\n'));
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import ErrorBoundaryFallback from './components/ErrorBoundaryFallback/ErrorBoundaryFallback.tsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorBoundaryFallback />}>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
